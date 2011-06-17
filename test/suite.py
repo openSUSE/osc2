ca4fd776fe3a0cf92dc21a40f3b321b958065143
Marcus Huewe <suse-tux@gmx.de>
2011-06-17 16:16:50 +0200

    - suite.py add test_build module

--- test/suite.py
+++ test/suite.py
@@ -3,9 +3,11 @@ import unittest
 
 import test_httprequest
 import test_remote
+import test_build
 
 suite = unittest.TestSuite()
 suite.addTests(test_httprequest.suite())
 suite.addTests(test_remote.suite())
+suite.addTests(test_build.suite())
 result = unittest.TextTestRunner(verbosity=1).run(suite)
 sys.exit(not result.wasSuccessful())
