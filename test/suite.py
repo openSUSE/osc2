ad2bc4c8dd81f0c93942bbeed64d964b8ec2b71b
Marcus Huewe <suse-tux@gmx.de>
2011-06-24 19:16:48 +0200

    - test/suite.py: changed import style

--- test/suite.py
+++ test/suite.py
@@ -1,13 +1,13 @@
 import sys
 import unittest
 
-import test.test_httprequest
-import test.test_remote
-import test.test_build
+from test import test_httprequest
+from test import test_remote
+from test import test_build
 
 suite = unittest.TestSuite()
-suite.addTests(test.test_httprequest.suite())
-suite.addTests(test.test_remote.suite())
-suite.addTests(test.test_build.suite())
+suite.addTests(test_httprequest.suite())
+suite.addTests(test_remote.suite())
+suite.addTests(test_build.suite())
 result = unittest.TextTestRunner(verbosity=1).run(suite)
 sys.exit(not result.wasSuccessful())
