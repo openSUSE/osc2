ef57eb628937f49e16a7e52450803fc2c112b3e0
Marcus Huewe <suse-tux@gmx.de>
2011-06-25 13:50:27 +0200

    - suite.py: added test_oscargs module to suite

--- test/suite.py
+++ test/suite.py
@@ -4,12 +4,14 @@ import unittest
 from test import test_httprequest
 from test import test_remote
 from test import test_build
+from test import test_oscargs
 from test.wc import test_util
 
 suite = unittest.TestSuite()
 suite.addTests(test_httprequest.suite())
 suite.addTests(test_remote.suite())
 suite.addTests(test_build.suite())
+suite.addTests(test_oscargs.suite())
 suite.addTests(test_util.suite())
 result = unittest.TextTestRunner(verbosity=1).run(suite)
 sys.exit(not result.wasSuccessful())
