a71354ce32b4e693ea292ed0530693459e076979
Marcus Huewe <suse-tux@gmx.de>
2011-07-10 22:33:31 +0200

    - test.suite: include testcases for test.wc.project and test.wc.package

--- test/suite.py
+++ test/suite.py
@@ -6,6 +6,8 @@ from test import test_remote
 from test import test_build
 from test import test_oscargs
 from test.wc import test_util
+from test.wc import test_project
+from test.wc import test_package
 
 suite = unittest.TestSuite()
 suite.addTests(test_httprequest.suite())
@@ -13,5 +15,7 @@ suite.addTests(test_remote.suite())
 suite.addTests(test_build.suite())
 suite.addTests(test_oscargs.suite())
 suite.addTests(test_util.suite())
+suite.addTests(test_project.suite())
+suite.addTests(test_package.suite())
 result = unittest.TextTestRunner(verbosity=1).run(suite)
 sys.exit(not result.wasSuccessful())
