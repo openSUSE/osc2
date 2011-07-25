c01a08f933eb6687fadd0e705881ebaedf303dd6
Marcus Huewe <suse-tux@gmx.de>
2011-07-25 10:46:01 +0200

    - test.suite: added test.test_source module

--- test/suite.py
+++ test/suite.py
@@ -4,6 +4,7 @@ import unittest
 from test import test_httprequest
 from test import test_remote
 from test import test_build
+from test import test_source
 from test import test_oscargs
 from test.wc import test_util
 from test.wc import test_project
@@ -13,6 +14,7 @@ suite = unittest.TestSuite()
 suite.addTests(test_httprequest.suite())
 suite.addTests(test_remote.suite())
 suite.addTests(test_build.suite())
+suite.addTests(test_source.suite())
 suite.addTests(test_oscargs.suite())
 suite.addTests(test_util.suite())
 suite.addTests(test_project.suite())
