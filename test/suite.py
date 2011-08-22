868835497d81ce2fda63aea3fb8d25126e609d2b
Marcus Huewe <suse-tux@gmx.de>
2011-08-22 16:16:00 +0200

    - test.suite: added additional_tests method which returns a unittest.TestSuite object
    Now it's possible to run the testsuite via "python setup.py test"

--- test/suite.py
+++ test/suite.py
@@ -11,15 +11,21 @@ from test.wc import test_project
 from test.wc import test_package
 from test.wc import test_convert
 
-suite = unittest.TestSuite()
-suite.addTests(test_httprequest.suite())
-suite.addTests(test_remote.suite())
-suite.addTests(test_build.suite())
-suite.addTests(test_source.suite())
-suite.addTests(test_oscargs.suite())
-suite.addTests(test_util.suite())
-suite.addTests(test_project.suite())
-suite.addTests(test_package.suite())
-suite.addTests(test_convert.suite())
-result = unittest.TextTestRunner(verbosity=1).run(suite)
-sys.exit(not result.wasSuccessful())
+
+def additional_tests():
+    suite = unittest.TestSuite()
+    suite.addTests(test_httprequest.suite())
+    suite.addTests(test_remote.suite())
+    suite.addTests(test_build.suite())
+    suite.addTests(test_source.suite())
+    suite.addTests(test_oscargs.suite())
+    suite.addTests(test_util.suite())
+    suite.addTests(test_project.suite())
+    suite.addTests(test_package.suite())
+    suite.addTests(test_convert.suite())
+    return suite
+
+if __name__ == '__main__':
+    suite = additional_tests()
+    result = unittest.TextTestRunner(verbosity=1).run(suite)
+    sys.exit(not result.wasSuccessful())
