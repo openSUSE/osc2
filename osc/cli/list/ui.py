7325caf34b80cce7da73dd08955f54d92f9025e7
Marcus Huewe <suse-tux@gmx.de>
2012-08-17 17:43:21 +0200

    - osc.cli.list: added initial version of the "list" subcommand

---
+++ osc/cli/list/ui.py
@@ -0,0 +1,30 @@
+"""Defines the review command."""
+
+from osc.cli.cli import OscCommand, call
+from osc.cli.description import CommandDescription, Option
+from osc.cli.list import list
+
+
+class List(CommandDescription, OscCommand):
+    """List projects, packages and files.
+
+    Examples:
+    osc list api://                 # list all projects
+    osc list api://project          # list all packages in project
+    osc list api://project/package  # list all files in package
+
+    """
+    cmd = 'list'
+    args = 'api://project?/package?'
+    use_wc = True
+    opt_verbose = Option('v', 'verbose', 'verbose listing (for files only)',
+                         action='store_true')
+    opt_expand = Option('e', 'expand', 'expand a source link',
+                        action='store_true')
+    opt_revision = Option('r', 'revision', 'list revision (for files only)',
+                          default='latest')
+    opt_deleted = Option('D', 'deleted', 'show deleted projects or packages',
+                         action='store_true')
+    opt_meta = Option('M', 'meta', 'list package\'s meta files',
+                      action='store_true')
+    func = call(list.list)
