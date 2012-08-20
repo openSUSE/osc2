7d70bc556e5d3e93c3a3c4533c017280d8a85a7f
Marcus Huewe <suse-tux@gmx.de>
2012-08-20 04:00:20 +0200

    - osc.cli.commit: added initial version of the commit command

---
+++ osc/cli/commit/ui.py
@@ -0,0 +1,22 @@
+"""Defines the commit command."""
+
+from osc.cli.cli import OscCommand, call
+from osc.cli.description import CommandDescription, Option
+from osc.cli.commit.commit import WCCommitController
+
+
+class Commit(CommandDescription, OscCommand):
+    """Update project or package.
+
+    Examples:
+    osc commit                          # in a project or package wc
+    osc commit project                  # update project
+    osc commit package                  # update package
+    osc commit /path/to/wc/or/wc/file   # update package
+
+    """
+    cmd = 'commit'
+    args = 'wc_path'
+    args_opt = [0]
+    opt_message = Option('m', 'message', 'specify a message')
+    func = call(WCCommitController().commit)
