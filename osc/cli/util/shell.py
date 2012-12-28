6def58b41a3ed8ef88a133adb553d16c0d796dc2
Marcus Huewe <suse-tux@gmx.de>
2012-12-28 05:44:46 +0100

    - osc.cli.util.shell: added class ItemSelector
    An item selector can be used to select one out of multiple
    items. It will prompt for an input and the item which corresponds
    to the input will be returned.
    This class will be used by the "review shell" in order to select a
    specific review which should be accepted, declined etc.
    
    Later this class will also be used to select a accept, decline etc.
    message from a list of (user specified) accept, decline etc. messages
    (for this task its <tab>-completion feature might come handy...)

--- osc/cli/util/shell.py
+++ osc/cli/util/shell.py
@@ -4,6 +4,7 @@ import readline
 import subprocess
 
 from osc.cli import parse
+from osc.cli.description import build_description, build_command
 
 
 class ShellSyntaxError(SyntaxError):
@@ -152,3 +153,106 @@ class AbstractShell(object):
     def run(self, *args, **kwargs):
         """Runs the shell with the specified args and kwargs."""
         raise NotImplementedError()
+
+
+class AbstractItemStorage(object):
+    """Represents a storage for multiple items.
+
+    Each item has an associated key. The str representation of
+    the key must not contain a whitespace.
+
+    """
+
+    def iteritems(self):
+        """Yields (key, item) tuples."""
+        raise NotImplementedError()
+
+    def __str__(self):
+        """Returns the str representation of the item storage."""
+        return super(AbstractItemStorage, self).__str__()
+
+
+class DictItemStorage(AbstractItemStorage):
+    """Stores items in a dict."""
+
+    def __init__(self):
+        """Constructs a new DictItemStorage object."""
+        super(DictItemStorage, self).__init__()
+        self._dict = {}
+
+    def add(self, key, item):
+        """Adds a new key, item pair to the storage.
+
+        If an item is already associated with the key key
+        it is replaced.
+
+        """
+        self._dict[key] = item
+
+    def iteritems(self):
+        return self._dict.iteritems()
+
+    def __iter__(self):
+        # just for convenience
+        return self._dict.__iter__()
+
+    def __str__(self):
+        return str(self._dict)
+
+
+class ItemSelector(AbstractShell):
+    """Represents an item selector.
+
+    An item selector can be used to select one out of multiple
+    items. It will prompt for an input and the item which corresponds
+    to the input will be returned.
+
+    """
+
+    def __init__(self, item_storage, **kwargs):
+        """Constructs a new ItemSelector object.
+
+        item_storage is subclass instance of AbstractItemStorage.
+        The str representation of each key must not contain a
+        whitespace character otherwise a ValueError is raised.
+        **kwargs are passed to the base class' __init__ method.
+
+        """
+        super(ItemSelector, self).__init__(**kwargs)
+        for k in item_storage:
+            if ' ' in str(k):
+                msg = "Key \"%s\" contains an illegal whitespace" % k
+                raise ValueError(msg)
+        self._item_storage = item_storage
+        self._build_cmds(item_storage)
+
+    def _build_cmds(self, item_storage):
+        """Builds a command hierarchy for the items."""
+        description_cls = build_description('ItemSelector', {})
+        self._root_cmd = build_command(description_cls)
+        for cmd, item in item_storage.iteritems():
+            attrs = {'cmd': cmd,
+                     'help_str': str(item),
+                     'func': staticmethod(lambda info: item)}
+            cls = build_command(description_cls, self._root_cmd, **attrs)
+
+    def _root_cmd_cls(self):
+        return self._root_cmd
+
+    def run(self):
+        """Runs the item selector"""
+        text = str(self._item_storage)
+        while True:
+            try:
+                inp = self.prompt(text=text)
+                text = ''
+                return self._execute(inp)
+            except SystemExit:
+                # argparse automatically exits when the
+                # help is requested
+                pass
+            except ShellSyntaxError as e:
+                self._renderer.render_text(str(e))
+            except KeyboardInterrupt as e:
+                msg = "Press to ctrl-D to exit"
+                self._renderer.render_text(msg)
