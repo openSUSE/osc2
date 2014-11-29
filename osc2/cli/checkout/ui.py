d5891ef6c4e3841081a1fd7174bfb5cc9f3cc147
Marcus Huewe <suse-tux@gmx.de>
2014-11-29 02:23:36 +0100

    - cli.checkout.ui: updated examples in the __doc__ str

--- osc2/cli/checkout/ui.py
+++ osc2/cli/checkout/ui.py
@@ -6,11 +6,18 @@ from osc2.cli.checkout.checkout import WCCheckoutController
 
 
 class Checkout(CommandDescription, OscCommand):
-    """Checkout project or package.
+    """Checkout project, package, or file.
 
     Examples:
-    osc checkout api://project          # checkout project
-    osc checkout api://project/package  # checkout package
+    osc2 checkout api://project                 # checkout project
+    osc2 checkout api://project/package         # checkout package
+    osc2 checkout /path/to/project              # revert all local
+                                                  modifications in the project
+                                                  working copy
+    osc2 checkout /path/to/project/package      # revert all local
+                                                  modifications in the package
+                                                  working copy
+    osc2 checkout /path/to/project/package/file # revert the file file
 
     """
     cmd = 'checkout'
