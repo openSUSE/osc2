df6ce264b7a9245fc93756b7b4bb38a61684b88b
Marcus Huewe <suse-tux@gmx.de>
2014-12-17 15:18:23 +0100

    - test.util.test_io: added testcases for the TemporaryDirectory class

---
+++ test/util/test_io.py
@@ -0,0 +1,166 @@
+import unittest
+import os
+import tempfile
+
+from osc2.util.io import TemporaryDirectory
+
+
+def suite():
+    return unittest.makeSuite(TestIO)
+
+
+class TestIO(unittest.TestCase):
+    def setUp(self):
+        # in order to avoid race conditions, all tmpdirs are created
+        # in a testcase specific tmpdir (actually, we cannot avoid race
+        # conditions, because another process could write into our tmpdir
+        # for whatever reason... but this is so unlikely:))
+        self._tmpdir = tempfile.mkdtemp(suffix='testio')
+
+    def tearDown(self):
+        # if the tmpdir is not empty, a testcase failed;
+        # so it does not harm if rmdir bails out as well
+        # (using shutil is too "dangerous")
+        os.rmdir(self._tmpdir)
+
+    def test_tmpdir1(self):
+        """simple temporary directory"""
+        tmpdir = TemporaryDirectory(dir=self._tmpdir)
+        path = tmpdir.path
+        self.assertIsNotNone(path)
+        self.assertTrue(os.path.isdir(path))
+        self.assertEqual(str(tmpdir), path)
+        # a simple assignment to path is not supposed to work, but _path
+        # can still be modified (_never_ do that)
+        self.assertRaises(AttributeError, setattr, tmpdir, 'path', path)
+        tmpdir.rmtree()
+        self.assertIsNone(tmpdir.path)
+        self.assertFalse(os.path.isdir(path))
+
+    def test_tmpdir2(self):
+        """simple temporary directory (remove with rmdir)"""
+        tmpdir = TemporaryDirectory(dir=self._tmpdir)
+        path = tmpdir.path
+        self.assertIsNotNone(path)
+        self.assertTrue(os.path.isdir(path))
+        tmpdir.rmdir()
+        self.assertIsNone(tmpdir.path)
+        self.assertFalse(os.path.isdir(path))
+
+    def test_tmpdir3(self):
+        """multiple remove"""
+        tmpdir = TemporaryDirectory(dir=self._tmpdir)
+        path = tmpdir.path
+        self.assertIsNotNone(path)
+        self.assertTrue(os.path.isdir(path))
+        tmpdir.rmtree()
+        self.assertFalse(os.path.isdir(path))
+        # a call to path does not recreate the tmpdir
+        self.assertIsNone(tmpdir.path)
+        self.assertFalse(os.path.isdir(path))
+        # recreate directory
+        os.mkdir(path)
+        self.assertTrue(os.path.isdir(path))
+        # subsequent rmdir/rmtree calls have no affect anymore
+        tmpdir.rmdir()
+        tmpdir.rmtree()
+        self.assertTrue(os.path.isdir(path))
+        os.rmdir(path)
+
+    def test_tmpdir4(self):
+        """test __del__ method"""
+        tmpdir = TemporaryDirectory(dir=self._tmpdir)
+        path = tmpdir.path
+        self.assertIsNotNone(path)
+        self.assertTrue(os.path.isdir(path))
+        del tmpdir
+        self.assertFalse(os.path.isdir(path))
+
+    def test_tmpdir5(self):
+        """test advanced __del__ semantics"""
+        tmpdir = TemporaryDirectory(dir=self._tmpdir)
+        ref = tmpdir
+        path = tmpdir.path
+        self.assertIsNotNone(path)
+        self.assertTrue(os.path.isdir(path))
+        # ref still references the TemporaryDirectory object
+        del tmpdir
+        self.assertTrue(os.path.isdir(path))
+        del ref
+        self.assertFalse(os.path.isdir(path))
+
+    def test_tmpdir6(self):
+        """test context manager"""
+        path = None
+        with TemporaryDirectory(dir=self._tmpdir) as tmpdir:
+            path = tmpdir.path
+            self.assertTrue(os.path.isdir(path))
+        self.assertIsNone(tmpdir.path)
+        self.assertFalse(os.path.isdir(path))
+
+    def test_tmpdir7(self):
+        """test context manager (double cleanup has no effect)"""
+        path = None
+        with TemporaryDirectory(dir=self._tmpdir) as tmpdir:
+            path = tmpdir.path
+            self.assertTrue(os.path.isdir(path))
+        self.assertIsNone(tmpdir.path)
+        self.assertFalse(os.path.isdir(path))
+        # also performs a rmtree
+        del tmpdir
+
+    def test_tmpdir8(self):
+        """test context manager (delete=False)"""
+        path = None
+        with TemporaryDirectory(dir=self._tmpdir, delete=False) as tmpdir:
+            path = tmpdir.path
+            self.assertTrue(os.path.isdir(path))
+        self.assertTrue(os.path.isdir(path))
+        self.assertIsNotNone(tmpdir.path)
+        del tmpdir
+        self.assertTrue(os.path.isdir(path))
+        os.rmdir(path)
+
+    def test_tmpdir9(self):
+        """test __enter__ (tmpdir was already deleted)"""
+        tmpdir = TemporaryDirectory(dir=self._tmpdir)
+        tmpdir.rmtree()
+        self.assertIsNone(tmpdir.path)
+        self.assertRaises(ValueError, tmpdir.__enter__)
+
+    def test_tmpdir10(self):
+        """do not remove tmpdir"""
+        tmpdir = TemporaryDirectory(dir=self._tmpdir, delete=False)
+        path = tmpdir.path
+        self.assertTrue(os.path.isdir(path))
+        del tmpdir
+        self.assertTrue(os.path.isdir(path))
+        os.rmdir(path)
+
+    def test_tmpdir11(self):
+        """test remove nonempty dir with rmdir=True"""
+        tmpdir = TemporaryDirectory(dir=self._tmpdir, rmdir=True)
+        path = tmpdir.path
+        self.assertTrue(os.path.isdir(path))
+        fname = os.path.join(path, 'foo')
+        with open(fname, 'w'):
+            pass
+        self.assertTrue(os.path.exists(fname))
+        self.assertRaises(OSError, tmpdir.__del__)
+        self.assertTrue(os.path.exists(fname))
+        os.unlink(fname)
+        del tmpdir
+        self.assertFalse(os.path.isdir(path))
+
+    def test_tmpdir12(self):
+        """test manual removal of the tmpdir"""
+        tmpdir = TemporaryDirectory(dir=self._tmpdir)
+        path = tmpdir.path
+        self.assertTrue(os.path.exists(path))
+        os.rmdir(path)
+        self.assertFalse(os.path.exists(path))
+        # as long as the directory is gone, we are fine
+        tmpdir.rmdir()
+
+if __name__ == '__main__':
+    unittest.main()
